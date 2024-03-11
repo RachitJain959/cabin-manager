277bd9b85f8890beb8387c1087480f356d66f4e3
Rachit Jain <rachitjain9596@gmail.com>
2024-03-11 23:50:38 +0530

    GlobalStyles p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
 `;
 
+const StyledApp = styled.div`
+  background-color: red;
+`;
+
 function App() {
   return (
-    <div>
-      <H1>The wild oasis</H1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The wild oasis</H1>
+      </StyledApp>
+    </>
   );
 }
 
