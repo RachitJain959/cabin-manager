789caa0149de4885ba31166e4925260c9e06d346
Rachit Jain <rachitjain9596@gmail.com>
2024-03-12 18:55:01 +0530

    bookings.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/dashboard" />
+        <Route index element={<Navigate replace to="dashboard" />} />
+
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="cabins" element={<Dashboard />} />
+        <Route path="users" element={<Dashboard />} />
+        <Route path="settings" element={<Dashboard />} />
+        <Route path="account" element={<Dashboard />} />
+        <Route path="login" element={<Dashboard />} />
+        <Route path="*" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
