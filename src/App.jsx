7384ef5b170892293a43a402c6edcd7b13e32468
Rachit Jain <rachitjain9596@gmail.com>
2024-03-12 18:49:02 +0530

    dashboard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
-
-const StyledApp = styled.div`
-  background-color: red;
-`;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <H1>The wild oasis</H1>
-      </StyledApp>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
