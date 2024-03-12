020297af4d6f092622565cd332808cbe7e6d5eed
Rachit Jain <rachitjain9596@gmail.com>
2024-03-12 18:56:46 +0530

    users.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
 import Settings from "./pages/Settings";
+import Users from "./pages/Users";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="bookings" element={<Bookings />} />
         <Route path="cabins" element={<Cabins />} />
-        <Route path="users" element={<Dashboard />} />
+        <Route path="users" element={<Users />} />
         <Route path="settings" element={<Settings />} />
         <Route path="account" element={<Dashboard />} />
         <Route path="login" element={<Dashboard />} />
