424ff10e3ac8a0b3f818c049aca70fdf1270985f
Rachit Jain <rachitjain9596@gmail.com>
2024-03-15 01:12:56 +0530

    delete cabin p1-onSuccess:invalidateQueries

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
 
 const TableRow = styled.div`
@@ -53,8 +53,13 @@ function CabinRow({ cabin }) {
     image,
   } = cabin;
 
-  const { isDeleting, mutate } = useMutation({
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: (id) => deleteCabin(id),
+    onSuccess: queryClient.invalidateQueries({
+      queryKey: ["cabins"],
+    }),
   });
 
   return (
