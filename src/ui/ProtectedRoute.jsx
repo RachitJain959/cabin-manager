67cd90de773c7e8e7dde56ed26f0102103e1bfa3
Rachit Jain <rachitjain9596@gmail.com>
2024-07-22 17:38:12 +0530

    authorization p3-userUser

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,7 +1,12 @@
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+
 function ProtectedRoute({ children }) {
   // 1. Load the authenticated user
+  const { isLoading, user } = useUser();
 
   // 2. while loading, show spinner.
+  if (isLoading) return <Spinner />;
 
   // 3. If there is NO authenticated user, redirect to /Login
 
