27ef99891093daec7ffe03a5d8c1b24c3bbd14cd
Rachit Jain <rachitjain9596@gmail.com>
2024-07-29 13:16:36 +0530

    dashboard p13-Duration CHart setup

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,8 +34,11 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
+
       <div>Today's activities</div>
-      <div>Charts</div>
+
+      <DurationChart confirmedStays={confirmedStays} />
+
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
