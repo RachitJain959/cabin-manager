84d3deac59d00d231308cdef5d17ce6cd64ec38d
Rachit Jain <rachitjain9596@gmail.com>
2024-07-30 12:36:01 +0530

    data item

--- src/features/bookings/BookingDataBox.jsx
+++ src/features/bookings/BookingDataBox.jsx
@@ -7,7 +7,7 @@ import {
   HiOutlineHomeModern,
 } from "react-icons/hi2";
 
-import DataItem from "../../../starter/ui/DataItem";
+import DataItem from "../../ui/DataItem";
 import { Flag } from "../../ui/Flag";
 
 import { formatDistanceFromNow, formatCurrency } from "../../utils/helpers";
