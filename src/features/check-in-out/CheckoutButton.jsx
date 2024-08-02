e9937bacaf53262fe1887d269e2500e0bb33bdf2
Rachit Jain <rachitjain9596@gmail.com>
2024-08-02 16:03:12 +0530

    Demo user todayActivity

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,14 +1,21 @@
+import toast from "react-hot-toast";
 import Button from "../../ui/Button";
+import { useUser } from "../authentication/useUser";
 import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
   const { checkout, isCheckingOut } = useCheckout();
+  const { user } = useUser();
 
   return (
     <Button
       variation="primary"
       size="small"
-      onClick={() => checkout(bookingId)}
+      onClick={() => {
+        user.id === "06d6733e-c5e1-42ea-b8b7-2a20deddfb2a"
+          ? toast.error("Demo User. Read only!")
+          : checkout(bookingId);
+      }}
       disabled={isCheckingOut}
     >
       Check out
