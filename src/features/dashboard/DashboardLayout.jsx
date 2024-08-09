68ddaa1f354164c35409159bcb33438d9fff0165
Rachit Jain <rachitjain9596@gmail.com>
2024-08-09 16:38:25 +0530

    demoTodayActivity

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
 import TodayActivity from "../check-in-out/TodayActivity";
+import DemoTodayActivity from "../check-in-out/DemoTodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -37,6 +38,7 @@ function DashboardLayout() {
       />
 
       <TodayActivity />
+      <DemoTodayActivity />
 
       <DurationChart confirmedStays={confirmedStays} />
 
