0f21acb2b749487a2fb948ce02d40a06b5f982cb
Rachit Jain <rachitjain9596@gmail.com>
2024-08-13 14:25:26 +0530

    demotodayactivity

---
+++ src/features/demo/DemoDashboardLayout.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { useRecentBookings } from "../dashboard/useRecentBookings";
+import { useRecentStays } from "../dashboard/useRecentStays";
+import Spinner from "../../ui/Spinner";
+import Stats from "../dashboard/Stats";
+import { useCabins } from "../cabins/useCabins";
+import SalesChart from "../dashboard/SalesChart";
+import DurationChart from "../dashboard/DurationChart";
+import DemoTodayActivity from "./DemoTodayActivity";
+
+const StyledDashboardLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto 34rem auto;
+  gap: 2.4rem;
+`;
+
+function DemoDashboardLayout() {
+  const { isLoading: isLoadingBookings, bookings } = useRecentBookings();
+  const {
+    isLoading: isLoadingStays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoadingCabins } = useCabins();
+
+  if (isLoadingBookings || isLoadingStays || isLoadingCabins)
+    return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+
+      <DemoTodayActivity />
+
+      <DurationChart confirmedStays={confirmedStays} />
+
+      <SalesChart bookings={bookings} numDays={numDays} />
+    </StyledDashboardLayout>
+  );
+}
+
+export default DemoDashboardLayout;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,24 @@
 import DashboardFilter from "../features/dashboard/DashboardFilter";
+import DemoDashboardLayout from "../features/demo/DemoDashboardLayout";
 import DashboardLayout from "../features/dashboard/DashboardLayout";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { useUser } from "../features/authentication/useUser";
 
 function Dashboard() {
+  const { user } = useUser();
+
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">Dashboard</Heading>
         <DashboardFilter />
       </Row>
-      <DashboardLayout />
+      {user.id === "06d6733e-c5e1-42ea-b8b7-2a20deddfb2a" ? (
+        <DemoDashboardLayout />
+      ) : (
+        <DashboardLayout />
+      )}
     </>
   );
 }
